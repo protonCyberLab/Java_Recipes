ecf642c4b636553afcbc6c9a70a7f8a9cc0a0627
Diesel Punk <cyberlab@protonmail.com>
2018-01-26 18:47:29 +0100

    Single dimension array example

---
+++ JavaBase/src/SingleDimensionArrayExample.java
@@ -0,0 +1,11 @@
+public class SingleDimensionArrayExample {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[]{1, 11, 2, 12, 3, 13, 4, 14};
+
+        //print the java int array
+        for (int i = 0; i <intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+}
